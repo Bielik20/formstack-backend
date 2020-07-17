7048123b55a7d4e3056a1453a8f4ce0248ef7467
Damian Bielecki <bielik20@gmail.com>
2020-07-17 19:35:53 +0200

    Make dynamodb work

--- src/handlers/get-all-items.js
+++ src/handlers/get-all-items.js
@@ -2,10 +2,11 @@
 
 // Get the DynamoDB table name from environment variables
 const tableName = process.env.SAMPLE_TABLE;
+const endpoint = process.env.AWS_DYNAMODB_ENDPOINT;
 
 // Create a DocumentClient that represents the query to add an item
 const dynamodb = require('aws-sdk/clients/dynamodb');
-const docClient = new dynamodb.DocumentClient();
+const docClient = new dynamodb.DocumentClient({endpoint});
 
 /**
  * A simple example includes a HTTP get method to get all items from a DynamoDB table.
